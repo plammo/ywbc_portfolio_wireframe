71a6ff8fb0df0a6a8c75b8eca8b0c960735886cf
plammo <plammo@live.com>
2014-07-10 15:50:20 +1200

    latest changes

---
+++ form_demo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Form Demo</title>
+</head>
+<body>
+	
+	<form method="post" action="success.php">
+
+		<label for="firstName">First Name: </label>
+		<input type="text" id="firstName" name="firstName">
+
+		<br>
+
+		<label for="lastName">Last Name: </label>
+		<input type="text" id="lastName" name="lastName">
+
+		<br>
+
+		<br>
+
+		<label>What is your favourite?</label>
+		<input type="radio" id="dog" name="dog" name="animals">
+
+
+		<br>
+
+
+
+
+</form>
+
+
+</body>
+</html>
