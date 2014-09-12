1f24b3fcf01fbb2f97449d028f8cfd994412c25e
plammo <plammo@live.com>
2014-09-12 16:11:35 +1200

    adding all files under wireframe

---
+++ includes/content.php
@@ -0,0 +1,287 @@
+<header>
+	<div class="row row_header clearfix col-sm-12">
+		<nav class="cl-effect-11">
+			<div class="row">
+				<div class="col-sm-2 logo">
+				<img src="images/pl_logo3.png" alt="pl logo" title="pic" class="img-responsive">
+				</div>
+           
+       			<ul class="col-sm-9 col-sm-offset-1">
+           			<li><a href="index.php" data-hover="Home">Home</a></li>
+           			<li><a href="aboutMe.php" data-hover="About">About</a></li>
+           			<li><a href="aboutMe.php" data-hover="Work">Work</a></li>
+           			<li><a href="aboutMe.php" data-hover="Family">Family</a></li>
+           			<li><a href="fitness.php" data-hover="Fitness">Fitness</a></li>
+           			<li><a href="contact.php" data-hover="Contact">Contact</a></li>
+       			</ul>
+        	</div>
+        </nav>
+    </div>
+</header>
+
+
+<main>
+
+	<section class="homepage">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1 welcome">
+				<h1>Hi, I'm Paul</h1>
+				<h2>Web designer, movie-watcher, husband, cat-daddy<br>
+				and all round good guy. Oh yeah, and I CrossFit...</h2>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3 intro">
+					<h1>About Me</h1>
+					<h2>As Mr Jagger once said - Pleased to meet you. Now, maybe you're more interested in what I can do than in who I am
+					 - and I know time's money.
+					<br><br>But when it comes to work I believe in people first,
+					 so I'd like to tell you a little bit about myself and why it would be great to work together.
+					  So please read on...</h2>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="tense_boxes">
+			<div class="row">
+				<div class="about_link link_type_1">
+					<h3><a href="#"><span data-hover="Where I've been">Past</span></a></h3>
+					<p>Info text Image</p>
+				</div>
+			
+			<div class="row row2">
+				<div class="about_link link_type_1">
+					<h3><a href="#"><span data-hover="Where I am">Present</span></a></h3>
+					
+					<p>Info text Image</p>
+				</div>
+
+				<div class="row row2">
+				<div class="about_link link_type_1">
+					<h3><a href="#"><span data-hover="Where I'm going">Future</span></a></h3>
+					
+					<p>Info text Image</p>
+				</div>
+
+				<div class="about_link col-1-3 about__topic--middle">
+					<h3>Present</h3>
+					<p>Info text image</p>
+				</div>
+			
+
+				<div class="about_link col-1-3 about__topic--right">
+					<h3>Future</h3>
+					<p>Info text Image</p>
+				</div>
+			</div> 
+		</div>
+	</section>
+
+	<section class="full-window debpic">
+
+		<div class="row col-sm-4 col-sm-offset-4">
+		<h1>This <span>is</span> my beautiful wife</h1>
+		</div>
+	</section>
+
+	<section class="full_window work">
+
+		<div class="container">
+			<div class="row row3">
+				<div class="col-1-1 intro">
+					<h1>Work, work, work</h1>
+					<h2>some intro information about the work I've done.
+					<br><br>some more information about the skills I have.<br><br>
+					some more information about why I'm doing this and what I want to achieve.</h2>
+				</div>
+			</div>
+
+			<div class="row row2">
+				<div class="col-1-3 work__topic--left">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--middle">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--right">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+
+				<div class="col-1-3 work__topic--left web_row2">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--middle web_row2">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--right web_row2">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			</div> 
+		</div>
+	</section>
+
+
+	<section class="full-window chaspic">
+
+		<div class="row col-sm-4 col-sm-offset-4">
+		<h1>This is Fat Chas.<br>
+		His skills are eating, sleeping and throwing mice in the air;<br>
+		he's a cat - what do you expect?</h1>
+		</div>
+	</section>
+
+
+	<section class="full_window work">
+
+		<div class="container">
+			<div class="row row3">
+				<div class="col-1-1 intro">
+					<h1>Mi Sagrada Familia</h1>
+					<h2>Family is everything.</h2>
+					<br><br>
+					<p>Deb is about the smartest, kindest, most dedicated person I know. She saved me.<br> We got married in 2002 at french farm, akaroa. moved to Nelson in 2004 <br>and about 2 years later our two boys came along, Dave and fat Chas.
+        <br><br>They're brothers from the same litter although they couldnt be more different.<br>Dave's a bit neurotic and needy and cant be without his family, whereas fat Chas is his own man and he has
+        <br>this mildly worrying look in his eye like you know he's thinking if only he were bigger, <br>he'd have a go at eating you.
+        </p>
+				</div>
+			</div>
+
+			<div class="row row2">
+				<div class="col-1-3 work__topic--left">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--middle">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--right">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+
+				<div class="col-1-3 work__topic--left web_row2">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--middle web_row2">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			
+
+				<div class="col-1-3 work__topic--right web_row2">
+					<h3>website title</h3>
+					<p>website Image</p>
+				</div>
+			</div> 
+		</div>
+	</section>
+
+
+
+
+				<!--<div class="col-1-2 para3">
+				<p>"Hi, I'm Paul. Pleased to meet you. Now, maybe you're more interested in what I can do than in who I am - and I know time's money.</p>
+				<p>But when it comes to work I believe in people first, so I'd like to tell you a little bit about myself and why it would be great to work together. So please read on...</p>
+				</div>
+		</div>
+
+		<div class="row row3">		
+						
+			<div class="col-1-2 para1">
+				<p>I live and work in Christchurch, New Zealand.
+           		<br>I have a beautiful wife, Deborah; she's an accountant.<br>We don't have children but we have 2 cats that might as well be.</p>
+           	</div>
+
+    	  	<div class="col-1-2 para2">
+           		<p>I've done a lot of things throughout my working life having had more than one career. Now I'm just breaking into the wide, wide world of the web as a web developer and I really enjoy it.</p>
+        	</div>
+
+					
+		</div>
+	
+	</section>-->
+
+	<!--<section class="section section__topic__content">
+		<div class="row row3">
+			<div class="col-1-3 topic__1__contentleft">
+				<h3>Content Block 1</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident earum animi incidunt nisi iure beatae sint, harum, nesciunt quibusdam quasi recusandae est minus suscipit debitis consectetur, inventore molestias obcaecati ad.</p>
+			</div>
+		
+
+			<div class="col-1-3 topic__1__contentmiddle">
+				<h3>Content Block 2</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident earum animi incidunt nisi iure beatae sint, harum, nesciunt quibusdam quasi recusandae est minus suscipit debitis consectetur, inventore molestias obcaecati ad.</p>
+			</div>
+
+			<div class="col-1-3 topic__1__contentright">
+				<h3>Content Block 3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident earum animi incidunt nisi iure beatae sint, harum, nesciunt quibusdam quasi recusandae est minus suscipit debitis consectetur, inventore molestias obcaecati ad.</p>
+			</div>
+		</div>
+	
+
+	</section>-->
+
+
+
+
+</main>
+	
+<footer>
+	<div class="row">
+
+		<div class="col-1-4 footer__contact">
+			<h4>Footer Details</h4>
+			<p>Contact</p>
+		</div>
+					
+		<div class="col-1-4 footer__contact">
+			<h4>Footer Details</h4>
+			<p>Message</p>
+		</div>
+	
+		<div class="col-1-4 footer__contact">
+			<h4>Footer Details</h4>
+			<p>SMedia</p>
+		</div>
+		
+		<div class="col-1-4 footer__contact">
+			<h4>Footer</h4>
+			<p>Links</p>
+		</div>
+	</div>
+</footer>
+</div>	
+</body>
+</html>
---
+++ includes/head.php
@@ -0,0 +1,19 @@
+<body>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Portfolio</title>
+
+	<!--<link rel="stylesheet" href="css/grid_styling.css">-->
+	<link rel="stylesheet" href="css/main.css">
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link href='http://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Amatic+SC:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,400,400italic' rel='stylesheet' type='text/css'>
+	<script src="js/modernizr.custom.js"></script>
+
+
+</head>
